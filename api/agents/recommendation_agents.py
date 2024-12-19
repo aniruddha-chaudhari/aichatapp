f72c64493c83d5325be84eefd901285e9cf0c336
aniruddha-chaudhari <140920431+aniruddha-chaudhari@users.noreply.github.com>
2024-12-20 00:01:40 +0530

    recommendation_agent done

--- api/agents/recommendation_agents.py
+++ api/agents/recommendation_agents.py
@@ -74,5 +74,79 @@ class RecommendationAgent():
                 break
 
         return recommendations 
+    
+    
+    def recommendation_classification(self,messages):
+        system_prompt = """ You are a helpful AI assistant for a coffee shop application which serves drinks and pastries. We have 3 types of recommendations:
+
+        1. Apriori Recommendations: These are recommendations based on the user's order history. We recommend items that are frequently bought together with the items in the user's order.
+        2. Popular Recommendations: These are recommendations based on the popularity of items in the coffee shop. We recommend items that are popular among customers.
+        3. Popular Recommendations by Category: Here the user asks to recommend them product in a category. Like what coffee do you recommend me to get?. We recommend items that are popular in the category of the user's requested category.
+        
+        Here is the list of items in the coffee shop:
+        """+ ",".join(self.products) + """
+        Here is the list of Categories we have in the coffee shop:
+        """ + ",".join(self.product_categories) + """
+
+        Your task is to determine which type of recommendation to provide based on the user's message.
+
+        Your output should be in a structured json format like so. Each key is a string and each value is a string. Make sure to follow the format exactly:
+        {
+        "chain of thought": Write down your critical thinking about what type of recommendation is this input relevant to.
+        "recommendation_type": "apriori" or "popular" or "popular by category". Pick one of those and only write the word.
+        "parameters": This is a  python list. It's either a list of of items for apriori recommendations or a list of categories for popular by category recommendations. Leave it empty for popular recommendations. Make sure to use the exact strings from the list of items and categories above.
+        }
+        """
+        input_messages = [{"role":"system","content":system_prompt}]+messages[-3:]
+
+        chat_response=  get_chatbot_response(self.client,input_messages)
+        output = self.post_process(chat_response)
+        return output
+    
+    def postprocess_classfication(self,output):
+        output = json.loads(output)
+
+        dict_output = {
+            "recommendation_type": output['recommendation_type'],
+            "parameters": output['parameters'],
+        }
+        return dict_output
+    
+    def get_recommendation_from_order(self,messages,order):
+        products = []
+        for product in order:
+            products.append(product['item'])
+
+        recommendations = self.get_apriori_recommendation(products)
+        recommendations_str = ",".join(recommendations)
+        system_prompt = f"""
+        You are a helpful AI assistant for a coffee shop application which serves drinks and pastries.
+        your task is to recommend items to the user based on their order.
+
+        I will provide what items you should recommend to the user based on their order in the user message. 
+        """
+        prompt = f"""
+        {messages[-1]['content']}
+        Please recommend me those items exactly: {recommendations_str}
+
+        """
+        messages[-1]['content'] = prompt
+
+        input_messages = [{"role":"system","content":system_prompt}]+messages[-3:]
+
+        chatbot_output = get_chatbot_response(self.client,input_messages)
+        output = self.post_process(chatbot_output)
+        return output
+    
+    def postprocess(self,output):
+        output = {
+            "role": "assistant",
+            "content": output,
+            "memory": {"agent":"recommendation_agent"
+                      }
+        }
+        return output
+
+      
 
         
