31a3b51dce2b5015b8c8ac7119ec1f7531ab5197
aniruddha-chaudhari <140920431+aniruddha-chaudhari@users.noreply.github.com>
2024-12-26 18:58:11 +0530

    just before the config

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { ImageBackground, SafeAreaView, Text, View } from "react-native";
+import { TouchableOpacity,GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GestureHandlerRootView>
+     <SafeAreaView className="w-full h-full">
+     <ImageBackground
+     className="w-full h-full flex justify-between"
+     source={require("../assets/images/bg.jpeg")}
+     >
+      <View>
+        <Text className="text-white text-center text-4xl mx-6 mt-24 font-[sora-bold]">Fall In Love With Coffee</Text>
+      </View>
+      
+      <TouchableOpacity
+      className="bg-[#c67c4e] h-16 mx-6 mb-10 rounded-lg items-center justify-center"
+      onPress={()=>{router.push('/(tabs)/Home')}}
+      >
+        <Text className="text-white text-center text-2xl font-[sora-bold]">Get Started</Text>
+      </TouchableOpacity>
+     </ImageBackground>
+     </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
--- app/babel.config.js
+++ app/babel.config.js
@@ -1,8 +1,9 @@
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: ["nativewind/babel"],
-    };
+// If merging configurations, remove this file and update the root babel.config.js accordingly.
+// Otherwise, ensure the 'plugins' property is correctly defined as an array.
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['nativewind/babel'],
   };
-  
+};
